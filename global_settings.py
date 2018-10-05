df59947ba860e769a7b3cd67e565e09dbddb400a
lyd1ng <lyding@protonmail.com>
2018-10-05 19:56:45 +0200

    Added comments to global_settings.py

--- global_settings.py
+++ global_settings.py
@@ -1,5 +1,12 @@
+"""
+This file defines global settings used all over the place.
+Their are not necessarily constants.
+"""
+
 SCREEN_DIMENSION = (1024, 728)
 MUSIC_VOLUME = 1.0
+# The prescaler defines the max music volume as the music should be
+# background music
 MUSIC_VOLUME_PRESCALER = 0.25
 SOUND_EFFECT_VOLUME = 1.0
 BOX_SIZE = (25, 25)
