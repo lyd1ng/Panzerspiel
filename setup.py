6e152f2bc5361d489ef6d4d2a306534ec38e5cdd
lyd1ng <lyding@protonmail.com>
2018-10-23 20:33:01 +0200

    Added pygame as a dependency

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version='0.9',
     packages=['panzerspiel', ],
     license='GNU General Public License v3.0',
+    install_requires=['pygame']
     long_description=open('README.md').read(),
     include_package_data=True,
     package_data={'panzerspiel': ['res2/*.*', 'res2/music/*.mp3']},
