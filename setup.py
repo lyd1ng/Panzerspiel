284fbfe778b69278a62c9cb4095ed1a01eae165c
lyd1ng <lyding@protonmail.com>
2018-10-23 21:25:52 +0200

    Made the setup.py working

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='Panzerspiel',
-    version='0.9',
+    version='1.00',
     packages=['panzerspiel', ],
     license='GNU General Public License v3.0',
-    install_requires=['pygame']
-    long_description=open('README.md').read(),
+    install_requires=['pygame'],
+    description="A small 2D tank game",
+    long_description=open('README.md', "r").read(),
+    long_description_content_type="text/markdown",
     include_package_data=True,
     package_data={'panzerspiel': ['res2/*.*', 'res2/music/*.mp3']},
     entry_points={
